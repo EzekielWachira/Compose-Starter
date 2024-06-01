3fd46c36f9f4afefa09dd8f8166673efd20f706c
Ezekiel Wachira <ezekielwachira048@gmail.com>
2024-06-01 15:15:49 +0300

    Initialize test-k2-compiler

---
+++ core/android/src/main/kotlin/dev/atick/core/extensions/FlowExtensions.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 Atick Faisal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.atick.core.extensions
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+
+/**
+ * Returns a [StateFlow] that represents the last value emitted by the [Flow]
+ *
+ * @param initialValue The initial value of the [StateFlow]
+ * @param scope The [CoroutineScope] to be used for the [StateFlow]
+ * @return A [StateFlow] that represents the last value emitted by the [Flow]
+ * */
+fun <T> Flow<T>.stateInDelayed(
+    initialValue: T,
+    scope: CoroutineScope,
+): StateFlow<T> {
+    return this.stateIn(
+        scope = scope,
+        initialValue = initialValue,
+        started = SharingStarted.WhileSubscribed(5000L),
+    )
+}
