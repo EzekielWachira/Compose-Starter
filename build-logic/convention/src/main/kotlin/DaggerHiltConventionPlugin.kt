f49db2f7be0d23ea45e540374ce8ce21a4a63222
Ezekiel Wachira <ezekielwachira048@gmail.com>
2024-06-01 15:15:54 +0300

    Initialize fix/#203

---
+++ build-logic/convention/src/main/kotlin/DaggerHiltConventionPlugin.kt
@@ -0,0 +1,23 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class DaggerHiltConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+            with(pluginManager) {
+                apply("com.google.dagger.hilt.android")
+                apply("com.google.devtools.ksp")
+            }
+
+            dependencies {
+                "implementation"(libs.findLibrary("dagger.hilt.android").get())
+                "ksp"(libs.findLibrary("dagger.hilt.compiler").get())
+            }
+        }
+    }
+}
